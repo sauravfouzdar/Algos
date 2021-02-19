79dc40918ed21c517611b086383d6961d149b038
Saurav Fouzdar <sauravfouzdar@gmail.com>
2021-02-19 13:47:12 +0530

    DFS added

---
+++ Graph/DFS.cpp
@@ -0,0 +1,12 @@
+vector<vector<int>> adj; // graph represented as an adjacency list
+int n; // number of vertices
+
+vector<bool> visited;
+
+void dfs(int v) {
+    visited[v] = true;
+    for (int child:child<adj[v].size(); ++child) {
+        if (!visited[child])
+            dfs(child);
+    }
+}
