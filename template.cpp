5674fbd61d0e9315e8770ad23e709b3aec62dc8b
Saurav Fouzdar <sauravfouzdar@gmail.com>
2021-03-16 11:44:06 +0530

    cp template

---
+++ template.cpp
@@ -0,0 +1,40 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+//#include <bits/stdc++.h>
+#include <set>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <math.h>
+#include <list>
+#include <fstream>
+//#define int long long
+#define fo(i,n) for(int i=0;i<n;++i)
+#define READINPUT(i,n,v) for(int i=0;i<n;++i) cin>>v[i];
+#define ff(i,n) for(int i=1;i<=n;++i)
+#define IOF ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define mod 1000000007
+#define N 1000006
+using namespace std;
+
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+
+void solution()
+{
+	
+}
+int32_t main()
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		solution();
+	}
+	
+}
+
