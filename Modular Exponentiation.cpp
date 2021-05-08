0084f2ce66ac63495e7462bba68e17116da9d5b1
Saurav Fouzdar <sauravfouzdar@gmail.com>
2021-05-08 12:28:58 +0530

    Update Modular Exponentiation.cpp

--- Modular Exponentiation.cpp
+++ Modular Exponentiation.cpp
@@ -1,17 +1,21 @@
-/* Iterative Function to calculate (x^y) in O(log y) */
-int power(int x, unsigned int y)
+int power(long long x, unsigned int y, int p)
 {
-	int res = 1;	 // Initialize result
-
-	while (y > 0)
-	{
-		// If y is odd, multiply x with result
-		if (y & 1)
-			res = res*x;
-
-		// y must be even now
-		y = y>>1; // y = y/2
-		x = x*x; // Change x to x^2
-	}
-	return res;
+    int res = 1;     // Initialize result
+ 
+    x = x % p; // Update x if it is more than or
+                // equal to p
+  
+    if (x == 0) return 0; // In case x is divisible by p;
+ 
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res*x) % p;
+ 
+        // y must be even now
+        y = y>>1; // y = y/2
+        x = (x*x) % p;
+    }
+    return res;
 }
